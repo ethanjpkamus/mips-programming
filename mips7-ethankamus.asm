49d2119c2912f2d1090862782dbfe6de62d50b10
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2022-03-22 09:42:44 -0700

    starting mips7

---
+++ mips7-ethankamus.asm
@@ -0,0 +1,16 @@
+# mips 7
+# Write a program with a subroutine that takes three 
+# arguments, A, X, and Y. It then computes A*X*Y and 
+# returns it.
+
+# The main method, in a loop, prompts the user for 
+# values of A, X and Y and prints out the result.
+	.data
+	.globl main
+	
+	.text
+main:	
+	
+end:
+	li      $v0, 10
+    syscall
